8b3465c643de9d6bdfa429ac30a54ca4d393b30a
Alain Boyer <boyer_al@yahoo.ca>
2024-10-19 10:44:27 +0300

    Fix custom timestamp format
    - Use provided format when computing infix from timestamp.
    - Add new rotate example.

---
+++ examples/rotate.rs
@@ -0,0 +1,28 @@
+use flexi_logger::{
+    Age, Cleanup, Criterion, Duplicate, FileSpec, FlexiLoggerError, LevelFilter, Logger, Naming,
+};
+use std::{thread::sleep, time::Duration};
+
+fn main() -> Result<(), FlexiLoggerError> {
+    Logger::with(LevelFilter::Info)
+        .rotate(
+            Criterion::Age(Age::Second),
+            Naming::TimestampsCustomFormat {
+                current_infix: None,
+                format: "%Y%m%d_%H%M%S",
+            },
+            Cleanup::Never,
+        )
+        .log_to_file(FileSpec::default())
+        .duplicate_to_stdout(Duplicate::All)
+        .start()?;
+
+    log::info!("start");
+    for step in 0..10 {
+        log::info!("step {}", step);
+        sleep(Duration::from_millis(250));
+    }
+    log::info!("done");
+
+    Ok(())
+}
