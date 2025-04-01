ab8e31e2321b39bbbfb6f73dd6c0b1a8f45e55ca
Meinolf Block <12658235+emabee@users.noreply.github.com>
2025-04-01 11:52:41 +0200

    Few renamings in write_writer.rs

--- examples/write_writer.rs
+++ examples/write_writer.rs
@@ -1,14 +1,14 @@
 use flexi_logger::writers::LogWriter;
 use std::{
-    io::{Error, ErrorKind},
+    io::Error,
     sync::{Arc, Mutex},
 };
 
 fn main() {}
 
 #[allow(dead_code)]
-struct MyWriter<F> {
-    file: Arc<Mutex<F>>,
+struct MyWriter<W> {
+    writer: Arc<Mutex<W>>,
 }
 
 impl<F: std::io::Write + Send + Sync> LogWriter for MyWriter<F> {
@@ -18,17 +18,17 @@ impl<F: std::io::Write + Send + Sync> LogWriter for MyWriter<F> {
         record: &flexi_logger::Record,
     ) -> std::io::Result<()> {
         let mut file = self
-            .file
+            .writer
             .lock()
-            .map_err(|e| Error::new(ErrorKind::Other, e.to_string()))?;
+            .map_err(|e| Error::other(e.to_string()))?;
         flexi_logger::default_format(&mut *file, now, record)
     }
 
     fn flush(&self) -> std::io::Result<()> {
         let mut file = self
-            .file
+            .writer
             .lock()
-            .map_err(|e| Error::new(ErrorKind::Other, e.to_string()))?;
+            .map_err(|e| Error::other(e.to_string()))?;
         file.flush()
     }
 }
